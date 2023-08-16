4a707077f09a11b074c6bd62856ec4568692b358
leanlp <leanlp@gmail.com>
2023-08-16 10:52:11 -0300

    response

--- src/smartwallet/smartwallet.controller.ts
+++ src/smartwallet/smartwallet.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Response } from '@nestjs/common';
 import { SmartwalletService } from "../smartwallet/smartwallet.service";
+
 
 // import {script} from "../../../enviormentKeywise/scripts/deploy.ts"
 
@@ -10,10 +11,8 @@ export class SmartwalletController {
   @Post('run')
   async runHardhatScript(
     @Body('param1') param1: string,
-    @Body('param2') param2: string,
-    @Body('param3') param3: string,
-    @Body('param4') param4: string,
-  ): Promise<string> {
-    return await this.smartwalletService.runScript(param1, param2, param3, param4);
+    ): Promise<string> {
+    
+    return await this.smartwalletService.runScript(param1);
   }
 }
--- src/smartwallet/smartwallet.service.ts
+++ src/smartwallet/smartwallet.service.ts
@@ -1,23 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Response } from '@nestjs/common';
 import { exec } from 'child_process';
+import { response } from 'express';
 
 @Injectable()
 export class SmartwalletService {
-    runScript(param1: string, param2: string, param3: string, param4: string): Promise<string> {
+    runScript(param1: string): Promise<string> {
         return new Promise((resolve, reject) => {
           const command = `npx hardhat run --network matic scripts/deploy.ts`;
 
           // Set the working directory to your Hardhat project root
           const options = {
-            cwd: '/home/oem/JOBS/keywise/enviormentKeywise'
+            cwd: '/home/oem/JOBS/keywise/enviormentKeywiseSendUserOp'      //in aws need to change to path nodeprocess
           };
 
-          exec(command, options, (error, stdout, stderr) => {
+          exec(command, options, async (error, stdout, stderr) => {
             if (error) {
               reject(`exec error: ${error}`);
               return;
             }
             resolve(stdout);
+            console.log("wallet", stdout);
           });
         });
     }
